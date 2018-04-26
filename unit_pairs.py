425434b5c5c3a227fd733d075d701b5adeb1f1e4
sijing <tusijing007@gmail.com>
2018-04-26 15:07:19 +0300

    .

--- unit_pairs.py
+++ unit_pairs.py
@@ -14,7 +14,7 @@ for n in range(4, 4):
     start_time = time.time()
     res = get_possible_mats(n)
     mx = [sum([sum(j) for j in i]) for i in res]
-    print "when n is %d the largest edges is %d\n" % (n, int(sum(mx)/2))
+    print "when n is %d the largest edges is %d\n" % (n, int(max(mx)/2))
     m = max(mx)
     print "there are %d cases\n" % len([i for i, j in enumerate(mx) if j == m])
     print "for example\n"
