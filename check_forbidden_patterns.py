eba7b6af0b401adc179412fa611949974777063a
sijing <tusijing007@gmail.com>
2018-04-26 12:19:25 +0300

    .

--- check_forbidden_patterns.py
+++ check_forbidden_patterns.py
@@ -3,8 +3,18 @@ import itertools
 
 
 def create_matrix(list_of_edges, n):
-    """Given half of all the edges and the number of nodes, create the matrix"""
-    f
+    """
+    Given half of all the edges and the number of nodes.
+    create the matrix
+    """
+    matrix = [[0 for i in range(n)] for j in range(n)]
+    ind = 0
+    for i in range(n):
+        for j in range(i+1):
+            matrix[i][j] = list_of_edges[ind]
+            matrix[j][i] = list_of_edges[ind]
+            ind += 1
+    return matrix
 
 
 def all_matrices(n):
