662b8d428a04d05fc75b334c583c41e826ed7a43
sijing <tusijing007@gmail.com>
2018-04-26 13:01:58 +0300

    .

--- check_forbidden_patterns.py
+++ check_forbidden_patterns.py
@@ -2,6 +2,13 @@
 import itertools
 
 
+def choose_2row_3colum(five):
+    list_of_two = list(itertools.combinations(five, 2))
+    tmp = [[list(set(five).difference(set(two))), two]
+           for two in list_of_two]
+    return tmp
+
+
 def create_matrix(list_of_edges, n):
     """
     Given half of all the edges and the number of nodes.
@@ -23,7 +30,17 @@ def all_matrices(n):
     complete = n * (n-1) / 2
     least = (n-1)*2 - 1  # the number of edges is at least 2(n-1)-1
     all_possible_list = [i for i in itertools.product([0, 1], repeat=complete)
-                         if sum(i) >= total_ones]
+                         if sum(i) >= least]
+    all_mats = [create_matrix(all_possible_list[i], n) for i in range(n)]
+    return all_mats
 
 
-#def check_k23
+def check_k23(list_of_mats, n):
+    """Return a list of mats that do not contain K23."""
+    five_list = list(itertools.combinations([i for i in range(n)], 5))
+    check_list = []
+    for five in five_list:
+        tmp = choose_2row_3colum()
+        for tp in tmp:
+            check_list.append(tp)
+    return check_list
